478778b7cec95d2b0c5c77e5062bb0efcc45a447
Nilesh <your-email@example.com>
2026-06-24 21:45:23 +0530

    update page

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://174.138.28.209:8000/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
